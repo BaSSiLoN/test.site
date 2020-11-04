1e82f87d76ffcaed7cb9dca8e5e5bf668eee1d71
BaSSiLoN <elias.kirsanidi@ya.ru>
2020-11-04 12:26:17 +0300

    remove extra php from header

--- parts/header.php
+++ parts/header.php
@@ -1,23 +1,3 @@
-<?php
-
-$current_page  = "";
-
-switch ($name) {
-    case "products":
-        $current_page = "Products";
-        break;
-    case "about":
-        $current_page = "About";
-        break;
-    case "contacts":
-        $current_page = "Contacts";
-        break;
-    default:
-        $current_page = "Main";
-}
-
-?>
-
 <header>
     <h1><?=$header?></h1>
     <h2><?=$subheader?></h2>
