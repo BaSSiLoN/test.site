a4792f0e9d9d6ee878a65de3788dceb0915b28ea
BaSSiLoN <elias.kirsanidi@ya.ru>
2020-11-04 13:21:30 +0300

    rewrite header and subheader generation

--- parts/code.php
+++ parts/code.php
@@ -1,7 +1,7 @@
 <?php
 
 // Get Current Page Name
-$name = isset($_GET["path"]) ? $_GET["path"] : "";
+$name = isset($_GET["path"]) ? $_GET["path"] : "main";
 
 // Get Title Name
 $title = "";
@@ -11,27 +11,5 @@ $result = mysqli_query($conn, $sql);
 while ($row = mysqli_fetch_assoc($result)) {
 
     $title = $row['title'];
-
-}
-
-// Get Header
-$header = "";
-$sql = "SELECT `header` FROM `menu` WHERE `link` = '" . $name . "'";
-$result = mysqli_query($conn, $sql);
-
-while ($row = mysqli_fetch_assoc($result)) {
-
-    $header = $row['header'];
-
-}
-
-// Get SubHeader
-$subheader = "";
-$sql = "SELECT `subheader` FROM `menu` WHERE `link` = '" . $name . "'";
-$result = mysqli_query($conn, $sql);
-
-while ($row = mysqli_fetch_assoc($result)) {
-
-    $subheader = $row['subheader'];
 
 }
--- parts/header.php
+++ parts/header.php
@@ -1,3 +1,29 @@
+<?php
+
+// Get Header
+$header = "";
+$sql = "SELECT `header` FROM `menu` WHERE `link` = '" . $name . "'";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    $header = $row['header'];
+
+}
+
+// Get SubHeader
+$subheader = "";
+$sql = "SELECT `subheader` FROM `menu` WHERE `link` = '" . $name . "'";
+$result = mysqli_query($conn, $sql);
+
+while ($row = mysqli_fetch_assoc($result)) {
+
+    $subheader = $row['subheader'];
+
+}
+
+?>
+
 <header>
     <h1><?=$header?></h1>
     <h2><?=$subheader?></h2>
